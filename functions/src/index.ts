d4792528885bdf91a0d208691bfb0bb331490eef
Gustavo Esteves <gustavoesteves@directon.com.br>
2025-02-09 22:28:26 -0300

    feat: log do recebimento da request e ajuste mock

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,5 @@
-import { onRequest } from "firebase-functions/v2/https";
 import * as logger from "firebase-functions/logger";
+import { onRequest } from "firebase-functions/v2/https";
 import { gerarRelatorio, mock } from "./helpers/GerarRelatorio";
 import { IRequestData } from "./models/IRequestData";
 
@@ -8,8 +8,10 @@ export const oncall_exportar_boleto_bancario = onRequest(
     logger.info("Cloud function oncall_exportar_boleto_bancario iniciada.");
 
     try {
-      const requestData: IRequestData = request.body ?? mock();
-      const html = await gerarRelatorio(requestData);
+      const requestData: IRequestData = request.body;
+      logger.info("Request data recebido: ", requestData?.beneficiario || 'N/A');
+
+      const html = await gerarRelatorio(mock());
 
       response.setHeader("Content-Type", "text/html");
       response.status(200).send(html);
